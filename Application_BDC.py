62e65e8a0c0c497f821f5bc7e9b740f26b9de87f
NicolasJulienData <128190647+NicolasJulienData@users.noreply.github.com>
2023-04-08 15:32:55 +0200

    Update Application_BDC.py

--- Application_BDC.py
+++ Application_BDC.py
@@ -81,13 +81,17 @@ if adresse != None:
     geo_response = requests.request("GET", GEOCODE_URL)
     geodata = json.loads(geo_response.text)
     try:
-     latlongmetro = [geodata['results'][0]['geometry']['location']['lat'],geodata['results'][0]['geometry']['location']['lng'],
-                    geodata['results'][0]['address_components'][2]["long_name"]]
+     lat_lon = pd.DataFrame{'lat':geodata['results'][0]['geometry']['location']['lat'],
+               'lon':geodata['results'][0]['geometry']['location']['lng']}
+     ville = geodata['results'][0]['address_components'][2]["long_name"]
     except IndexError:
-     latlongmetro = None
+     lat_lon = None
+     ville = None
      st.write('Adresse non trouvée')
 
-st.write(latlongmetro)
+if lat_lon != None:
+    st.write(lat_lon, ville)
+    st.map(data=lat_lon)
 
 # Input adresse
 # API Google Maps
